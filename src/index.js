fab0956b764033b88d7419a7910027c8b08a0b70
Luke Fairchild <lukecfairchild@gmail.com>
2019-05-01 14:30:27 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+
+const forwardHeaders = (incoming, outgoing) => {
+	const headers = [
+		// Unicity Headers
+		'x-request-uuid',
+		'x-request-release',
+
+		// Envoy Headers
+		'x-ot-span-context',
+		'x-request-id',
+
+		// Zipkin Headers
+		'x-b3-traceid',
+		'x-b3-spanid',
+		'x-b3-parentspanid',
+		'x-b3-sampled',
+
+		// Jaeger Headers
+		'x-b3-flags'
+	];
+
+	for (let i in headers) {
+		if (incoming.headers[headers[i]] === undefined) {
+			outgoing.headers[headers[i]] = incoming.headers[headers[i]]
+		}
+	}
+}
+
+
+module.exports = forwardHeaders
